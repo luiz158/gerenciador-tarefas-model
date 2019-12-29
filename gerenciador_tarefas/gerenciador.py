9e58275ae0632e5761df6ef82932a49e991e08bb
cassiobotaro <cassiobotaro@gmail.com>
2019-12-29 10:42:05 -0300

    adicionando funcionalidade de criar tarefas

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,10 +1,38 @@
+from enum import Enum
+from uuid import UUID, uuid4
+
 from fastapi import FastAPI
+from pydantic import BaseModel, constr
+from starlette.status import HTTP_201_CREATED
 
 
 app = FastAPI()
 TAREFAS = []
 
 
+class EstadosPossiveis(str, Enum):
+    finalizado = "finalizado"
+    nao_finalizado = "não finalizado"
+
+
+class TarefaEntrada(BaseModel):
+    titulo: constr(min_length=3, max_length=50)
+    descricao: constr(max_length=140)
+    estado: EstadosPossiveis = EstadosPossiveis.nao_finalizado
+
+
+class Tarefa(TarefaEntrada):
+    id: UUID
+
+
 @app.get("/tarefas")
 def listar():
     return TAREFAS
+
+
+@app.post("/tarefas", response_model=Tarefa, status_code=HTTP_201_CREATED)
+def criar(tarefa: TarefaEntrada):
+    nova_tarefa = tarefa.dict()
+    nova_tarefa.update({"id": uuid4()})
+    TAREFAS.append(nova_tarefa)
+    return nova_tarefa
